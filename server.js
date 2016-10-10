e325489d3a8f6617ea8672b84e19f2b583ed2dd7
kvanoop89 <kvanoop1989@gmail.com>
2016-10-10 16:39:32 +0200

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile("article one requested will be served here");
 });
 
 app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile("article two requested will be served here");
 });
 
 app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile("article three requested will be served here");
 });
 
 app.get('/ui/style.css', function (req, res) {
